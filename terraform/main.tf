f995339b654fd23c3b550f02ea05139ddf0ef032
Álvaro Rivas <alvarorivasalvarez@gmail.com>
2024-12-12 21:49:29 +0100

    Terraform

---
+++ terraform/main.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "tasks_app" {
+  ami           = "ami-0abcdef1234567890" # AMI de Amazon Linux o Ubuntu
+  instance_type = "t2.micro"
+  key_name      = var.key_name
+
+  tags = {
+    Name = "TasksAppInstance"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update -y",
+      "sudo apt-get install -y nodejs npm",
+      "cd /home/ubuntu/app && npm install && npm run build",
+      "pm2 start npm --name tasks-app -- start"
+    ]
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file(var.private_key_path)
+      host        = self.public_ip
+    }
+  }
+}
