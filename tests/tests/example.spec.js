c08e49e161fe5300be78313933ef9401ab1b21c0
Álvaro Rivas <alvarorivasalvarez@gmail.com>
2024-12-12 21:51:29 +0100

    Terraform

--- tests/tests/example.spec.js
+++ tests/tests/example.spec.js
@@ -1,9 +1,6 @@
-// @ts-check
 const { test, expect } = require('@playwright/test');
 
 test('has title', async ({ page }) => {
   await page.goto('localhost:3000');
-
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Tasks/);
 });
